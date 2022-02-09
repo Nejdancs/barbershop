2c47c6bb2015edd4d1251d416cf9195a4508cddb
Sergey Korobka <nejdancs@gmail.com>
2022-02-09 04:24:15 +0200

    Делает адаптив

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+  $('.slider__items').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: 'linear',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button class="slider__control slider__control_prev" href="#" >Наза<br />д</button>',
+    nextArrow:
+      '<button class="slider__control slider__control_next" href="#" >Впере<br />д</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 600,
+        settings: {},
+      },
+      {
+        breakpoint: 480,
+        settings: {},
+      },
+    ],
+  });
+});
